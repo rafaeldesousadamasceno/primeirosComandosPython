d91b891a6a86f5945b72373012af53758bf1a0fe
Rafael Damasceno <rafaelsd389@gmail.com>
2023-03-25 12:34:12 -0300

    Olá Mundo em Python + Lista de Exercícios

---
+++ exercicios.py
@@ -0,0 +1,52 @@
+print(">"*20 + " LISTA DE EXERCÍCIOS " + "<"*20)
+# print("Abaixo descrevo uma lista de exercícios: ")
+# Crie um programa que que imprima na tela uma frase de reflexão (Olá mundo!)
+
+# Imprima uma mensagem com o valor salvo dentro de uma variável
+
+# Colete o nome, a idade e a profissão de uma pessoa e imprima na tela num formato 
+#   "Olá {nome}, você tem {idade} e trabalha com {profissão}"
+
+# Colete o DIA, MÊS e ANO do nascimento de uma pessoa e depois mostre no seguinte formato
+#   "Você nasceu em {DIA}, no mês {MÊS} do ano de {ANO}"
+
+# Colete o DIA, MÊS e ANO do nascimento de uma pessoa e depois mostre no seguinte formato
+#   "DATA DE NASCIMENTO: DD/MM/AAAA"
+
+# Crie um programa que descreva detalhes de um dado digitado pelo usuário (dissecando uma variável)
+#   "O tipo de dado de entrada, se é número, ou alfanumérico, ou espaço, ou maiúscula, ou minúsculas"
+
+# Crie um programa que soma dois números e imprima na tela a soma
+
+# Crie um programa que mostre o antecessor de um número e o sucessor
+#   "Analisando o número {NUM}: Antecessor é {ANT} e o Sucessor é {SUC}"
+
+# Crie um programa que calcule o dobro, o triplo e a raiz quadrada
+
+# Crie um programa que calcule a média aritmética
+
+# Crie um programa que converta {real para dólar} e de {dólar para real}
+
+# Crie um programa que converta de graus {Célsius para Farenhit}
+
+# Crie um programa que converta um valor em Metros para:
+#   Km
+#   cm
+
+# Crie um programa que calcule a tabuada de um número
+#   {NUM} {+,-,*,/} [0-10]
+
+# Crie um programa que calcule quantos litros de tinta pinta uma parede dada as entradas de altura e largura
+#   Sabendo que 1 Litro pinta 2m²
+
+# Crie um programa que calcule quanto um funcionário vai receber de reajuste baseado no reajuste salarial anual
+
+# Crie um programa que calcule o preço com desconto de 5%
+
+# Crie um programa que calcule o preço final de um produto parcelado e a diferença do valor à vista
+
+# Crie um programa que Converta um valor de Litros para mL
+
+# Crie um programa que calcule a exponenciação de um número elevado a outro número
+
+# Crie um programa que calcule a soma de 1 - 100
